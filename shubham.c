dc5ae8d66d807976f63d1ca253894296471a8606
micro-shubham <shubhamtikkha09@gmail.com>
2022-11-12 13:10:20 +0530

    initial commit

--- shubham.c
+++ shubham.c
@@ -1,22 +1,16 @@
 #include <stdio.h>
 /* square of any number */
 int main()   {
-int n ,b ,i;
-printf("enter numbr ");
-scanf("%d",&n);
-for(i=1; i<=n; i=i+1){
-    b= i*i;
-    printf("%d\n",b);
+int i ,n ,b;
+i = 1;
+printf("enter number ");
+scanf(" %d",& n);
+while( i <=n )  {
+    b =i*i;
+printf("%d\n",b);
+i++;
+
 
 }
-
-
-
-
-
-
 return 0;
-
-
-
 }
