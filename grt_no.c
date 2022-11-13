7aef16513c20079b80902e18456cdecca351ab83
micro-shubham <shubhamtikkha09@gmail.com>
2022-11-13 21:29:28 +0530

    first commit

---
+++ grt_no.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main ()    {
+
+int a , b ;
+printf("enter a \n");
+scanf("%d", &a);
+printf("enter b \n");
+scanf("%d",&b);
+if(a>b ) {
+printf("a is greater number \n");
+
+
+}  
+
+else {
+   printf("b is greater number\n ");
+
+}
+
+return 0;
+}
