083fbb2debada33666f7ad891382aea3517f7734
micro-shubham <shubhamtikkha09@gmail.com>
2022-11-15 19:26:49 +0530

    first commit

---
+++ problem_2.c
@@ -0,0 +1,22 @@
+// check wheather number is positive, negative or zero//
+#include <stdio.h>
+int main ()  {
+int a ;
+printf("enter number a");
+scanf("%d",&a);
+if(a>0)  {
+printf("a is positive number  ");
+
+}
+if (a<0) {
+    printf("a is negative number ");
+ 
+}
+if (a==0) {
+printf("a is equal to zero ");
+}
+return 0;
+
+
+
+}
