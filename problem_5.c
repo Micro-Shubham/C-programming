7ae1c5d02dfef7cb7f5cbbef419d38fcc02009ad
micro-shubham <shubhamtikkha09@gmail.com>
2022-12-03 22:29:23 +0530

    first commit

---
+++ problem_5.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main ()  {
+int a,c,i;
+printf("enter number  ");
+scanf("%d",&a);
+i =1;
+while (i<=10) {
+c=a*i;
+printf("%d*%d = %d \n",a,i,c);
+i++;
+
+}
+
+
+return 0;
+
+
+
+
+}
