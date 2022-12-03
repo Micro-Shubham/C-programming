4692bfc40f4247ab3945f89464f50259f66330bb
micro-shubham <shubhamtikkha09@gmail.com>
2022-12-03 22:40:02 +0530

    first commit

--- loop.c
+++ loop.c
@@ -1,8 +1,12 @@
 #include<stdio.h>
 int main()
 {
-for(int i=1;i<=10;i=i+1)  {
-printf("%d\n",i);
+    int a,c;
+    printf("enter number ");
+    scanf("%d",&a);
+for(int i=1;i<=10;i++)  {
+c=a*i;
+printf("%d * %d =%d\n",a,i,c);
 
 }
 
